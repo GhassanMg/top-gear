cf42e29a748fc69f153c1971cc2f2597f58ab131
Silver-Bullet-11 <78919756+Silver-Bullet-11@users.noreply.github.com>
2022-05-20 14:41:48 +0300

    add car routes for web

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MessageController;
+use App\Http\Controllers\Admin\CarController;
 use Illuminate\Http\Request;
 use App\Models\Message;
 use Illuminate\Support\Facades\Route;
@@ -23,8 +24,12 @@ Route::get('/', [HomeController::Class , 'welcome'])->name('home');
 Route::view('/about', 'pages.about')->name('about');
 Route::view('/contact', 'pages.contact')->name('contact');
 
-Route::post('/contact', [MessageController::Class,'store'])->name('messages.store');
-Route::get('/admin/messages', [MessageController::Class,'index'])->name('messages.index');
-Route::get('/admin/messages/{message}', [MessageController::Class,'show'])->name('messages.show');
+Route::post('/contact', [MessageController::Class, 'store'])->name('messages.store');
 
+Route::get('/admin/messages', [MessageController::Class, 'index'])->name('messages.index');
+Route::get('/admin/messages/{message}', [MessageController::Class, 'show'])->name('messages.show');
 
+Route::get('/admin/cars', [CarController::Class, 'index'])->name('cars.index');
+Route::get('/admin/cars/create', [CarController::class, 'create'])->name('cars.create');
+Route::get('/admin/cars/{car}', [CarController::Class, 'show'])->name('cars.show');
+Route::post('/admin/cars', [CarController::Class, 'store'])->name('cars.store');
